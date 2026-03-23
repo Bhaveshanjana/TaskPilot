ee02e5d0dc1880cde0f309fe6b4c8a4c2057fded
Bhavesh <bhaveshanjana58@gmail.com>
2026-03-23 11:30:21 +0530

    feat(theme): replace old theme toggler with context-api

---
+++ client/src/context/ThemeProvider.jsx
@@ -0,0 +1,40 @@
+import {
+  Children,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme:dark)").matches),
+  );
+
+  const toggleTheme = () => {
+    setDarkMode((mode) => !mode);
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+  return (
+    <ThemeContext.Provider value={{ toggleTheme, darkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { OrganizationProvider } from "./context/OrganizationContext.jsx";
+import { ThemeProvider } from "./context/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <OrganizationProvider>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </OrganizationProvider>
     </BrowserRouter>
   </StrictMode>,
